b6992adb5ae4d12b902367fb3a4bc77d6edf2622
zohaib <zohaibs1997@gmail.com>
2022-07-21 20:48:55 +0100

    I have created a function called passwordGenerator which will generate the password, also assigned some variable with promp value

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function passwordGenerator() {
+  let passLength = prompt("Enter password length (between 8 to 128)");
+  let lowerCase = prompt(
+    "Do you want to include lower case character/s on your password?)"
+  );
+  let upperCase = prompt(
+    "Do you want to include upper case character/s on your password?)"
+  );
+  let number = prompt("Do you want to include number/s on your password?)");
+  let specialChar = prompt("Do want to special character/s on your password?)");
+  let alphabet = "abcdefghijklmnopqrstuvwxyz"; //this code line of code was taken form https://www.coderrocketfuel.com/article/generate-a-random-letter-from-the-alphabet-using-javascript
 }
 
 // Add event listener to generate button
