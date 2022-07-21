bb81d12e2d0f1d2614ae371ee29c93119e15a3ec
zohaib <zohaibs1997@gmail.com>
2022-07-21 21:48:30 +0100

    created a for loop to loop character generator until user's derised password length was reached

--- script.js
+++ script.js
@@ -22,18 +22,24 @@ function generatePassword() {
   const alphabet = "abcdefghijklmnopqrstuvwxyz"; //this code line of code was taken form https://www.coderrocketfuel.com/article/generate-a-random-letter-from-the-alphabet-using-javascript
 
   let generated = [];
-  if (lowerCase === "yes") {
-    let randomLowerCase = alphabet[Math.floor(Math.random() * alphabet.length)];
-    generated.push(randomLowerCase.toLowerCase());
+
+  for (let i = 0; i <= passLength.length; i++) {
+    if (lowerCase === "yes") {
+      let randomLowerCase =
+        alphabet[Math.floor(Math.random() * alphabet.length)]; //this code line of code was taken form https://www.coderrocketfuel.com/article/generate-a-random-letter-from-the-alphabet-using-javascript
+      generated.push(randomLowerCase.toLowerCase().toString());
+    }
+    if (upperCase === "yes") {
+      let randomUpperCase =
+        alphabet[Math.floor(Math.random() * alphabet.length)];
+      generated.push(randomUpperCase.toUpperCase().toString());
+    }
+    if (number === "yes") {
+      let randomNumber = Math.floor(Math.random() * 11);
+      generated.push(randomNumber.toString());
+    }
   }
-  if (upperCase === "yes") {
-    let randomUpperCase = alphabet[Math.floor(Math.random() * alphabet.length)];
-    generated.push(randomUpperCase.toUpperCase());
-  }
-  if (number === "yes") {
-    let randomNumber = Math.floor(Math.random() * 11);
-    generated.push(randomNumber.toString());
-  }
+
   console.log(generated);
 
   return generated;
