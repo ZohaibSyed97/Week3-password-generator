c4026f372d3210eab5bb2ec61343ca07bc63220b
zohaib <zohaibs1997@gmail.com>
2022-07-21 21:11:36 +0100

    created if statements to print random value if the prompt is yes (added criteria)

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function passwordGenerator() {
+function generatePassword() {
   let passLength = prompt("Enter password length (between 8 to 128)");
   let lowerCase = prompt(
     "Do you want to include lower case character/s on your password?)"
@@ -19,8 +19,26 @@ function passwordGenerator() {
   );
   let number = prompt("Do you want to include number/s on your password?)");
   let specialChar = prompt("Do want to special character/s on your password?)");
-  let alphabet = "abcdefghijklmnopqrstuvwxyz"; //this code line of code was taken form https://www.coderrocketfuel.com/article/generate-a-random-letter-from-the-alphabet-using-javascript
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"; //this code line of code was taken form https://www.coderrocketfuel.com/article/generate-a-random-letter-from-the-alphabet-using-javascript
+
+  let generated = [];
+  if (lowerCase === "yes") {
+    let randomLowerCase = alphabet[Math.floor(Math.random() * alphabet.length)];
+    generated.push(randomLowerCase.toString());
+  }
+  if (upperCase === "yes") {
+    let randomUpperCase = alphabet[Math.floor(Math.random() * alphabet.length)];
+    generated.push(randomUpperCase.toString());
+  }
+  if (number === "yes") {
+    let randomNumber = Math.floor(Math.random() * 11);
+    generated.push(randomNumber.toString());
+  }
+  console.log(generated);
+
+  return generated;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
